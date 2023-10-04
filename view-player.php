6bf8c78f89aecb11e9916cee4e18df1c0c2da95c
HustonBryan319 <142837335+HustonBryan319@users.noreply.github.com>
2023-10-04 05:03:19 -0500

    Update view-player.php

--- view-player.php
+++ view-player.php
@@ -16,7 +16,7 @@ while ($player = $player->fetch_assoc()) {
   <tr>
     <td><?php echo $player['player_id'];?></td>
     <td><?php echo $player['player_name'];?></td>
-    <td><?php echo $player['player_team'];?></td>
+    <td><?php echo $player['player_position'];?></td>
     <td><a href = "team-by-player.php?id=<?php echo $player['player_id'];?>">team</a></td>
   </tr>
   <?php
