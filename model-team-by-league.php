e9815c5c9fbe4aa6f7daacc8bda96e068e206e90
HustonBryan319 <142837335+HustonBryan319@users.noreply.github.com>
2023-10-04 04:40:13 -0500

    Update model-team-by-league.php

--- model-team-by-league.php
+++ model-team-by-league.php
@@ -1,8 +1,8 @@
 <?php
-function selectplayerbyteam($pid) {
+function selectteambyleague($tid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT t.team_id, team_name, team_leauge, country FROM `team` t join league l on l.team_id = t.team_id where l.player_id=?");
+        $stmt = $conn->prepare("SELECT t.team_id, team_name, team_leauge, country FROM `team` t join league l on l.team_id = t.team_id where l.league_id=?");
        $stmt->bind_param("i", $pid);
       $stmt->execute();
         $result = $stmt->get_result();
