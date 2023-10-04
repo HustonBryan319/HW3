e38bf03409e57d4353c33cb28ad565f184e592f5
HustonBryan319 <142837335+HustonBryan319@users.noreply.github.com>
2023-10-04 04:00:24 -0500

    Create model-team-by-player.php

---
+++ model-team-by-player.php
@@ -0,0 +1,16 @@
+<?php
+function selectplayerbyteam($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT t.team_id, team_name, team_leauge, country FROM `team` t join league l on l.team_id = t.team_id where l.player_id=?");
+       $stmt->bind_param("i", $pid);
+      $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
