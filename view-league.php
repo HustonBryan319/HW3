63d8516a2e5d71a3da63126b4d891aba58596bf3
HustonBryan319 <142837335+HustonBryan319@users.noreply.github.com>
2023-10-04 04:18:46 -0500

    Update view-league.php

--- view-league.php
+++ view-league.php
@@ -1,1 +1,28 @@
-r
+<h1>player</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead> 
+      <tr>
+         <th>ID</th>
+      <th>ID</th>
+      <th>ID</th>
+        <th>country</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+while ($player = $player->fetch_assoc()) {
+  ?>
+  <tr>
+    <td><?php echo $league['league_id'];?></td>
+    <td><?php echo $league['player_id'];?></td>
+    <td><?php echo $league['team_id'];?></td>
+    <td><?php echo $league['country'];?></td>
+  </tr>
+  <?php
+  
+}
+?>
+    </tbody>
+  </table>
+</div>
