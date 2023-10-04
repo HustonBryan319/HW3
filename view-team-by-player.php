6733dd24cd31dcd0298f18581a5eb1f0c4c7c89c
HustonBryan319 <142837335+HustonBryan319@users.noreply.github.com>
2023-10-04 04:05:22 -0500

    Create view-team-by-player.php

---
+++ view-team-by-player.php
@@ -0,0 +1,28 @@
+<h1>team by player</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead> 
+      <tr>
+         <th>ID</th>
+      <th>Name</th>
+      <th>league</th>
+        <th>country</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+while ($team = $team->fetch_assoc()) {
+  ?>
+  <tr>
+    <td><?php echo $team['team_id'];?></td>
+    <td><?php echo $team['team_name'];?></td>
+    <td><?php echo $team['team_league'];?></td>
+    <td><?php echo $team['country'];?></td>
+  </tr>
+  <?php
+  
+}
+?>
+    </tbody>
+  </table>
+</div>
