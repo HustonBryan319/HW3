044b91995a089ec8ccbf49a206036494f4e37a7a
HustonBryan319 <142837335+HustonBryan319@users.noreply.github.com>
2023-10-04 04:33:29 -0500

    Update team-by-league.php

--- team-by-league.php
+++ team-by-league.php
@@ -4,7 +4,7 @@ require_once("model-team-by-league.php");
 
 $pageTitle = "team by league";
 include "view-header.php";
-$team = selectteambyleague();
+$league = selectteambyleague($_POST['tid']);
 include "view-team-by-league.php"; 
 include "view-footer.php";
 ?>
