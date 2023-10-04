e94f5b066b69afe4ee7727641259df657199eaf2
HustonBryan319 <142837335+HustonBryan319@users.noreply.github.com>
2023-10-04 04:30:28 -0500

    Update team-by-league.php

--- team-by-league.php
+++ team-by-league.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-team-by-league.php");
 
-$pageTitle = "league";
+$pageTitle = "team by league";
 include "view-header.php";
 $team = selectteambyleague();
 include "view-team-by-league.php"; 
