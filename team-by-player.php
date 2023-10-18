58b84558e7eb5ec806553908fc0c13996cbefd7d
HustonBryan319 <142837335+HustonBryan319@users.noreply.github.com>
2023-10-18 14:23:34 -0500

    Update team-by-player.php

--- team-by-player.php
+++ team-by-player.php
@@ -4,7 +4,7 @@ require_once("model-team-by-player.php");
 
 $pageTitle = "team-by-player";
 include "view-header.php";
-$player = selectteambyplayer($_GET['id']);
+$player = selectteambyplayer($_GET['team_id']);
 include "view-team-by-player.php"; 
 include "view-footer.php";
 ?>
