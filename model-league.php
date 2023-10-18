e533321d58b6fd2fb5d97c034b6f0588bf620013
HustonBryan319 <142837335+HustonBryan319@users.noreply.github.com>
2023-10-18 17:44:23 -0500

    Update model-league.php

--- model-league.php
+++ model-league.php
@@ -2,7 +2,7 @@
 function selectleague() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT * FROM `league`");
+        $stmt = $conn->prepare("SELECT league_id, player_id, country, team_id FROM `league`");
        
       $stmt->execute();
         $result = $stmt->get_result();
